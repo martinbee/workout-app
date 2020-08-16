5fd52680627aa0ebeabefeceeacb0c4492c59991
Martin <martbee88@gmail.com>
2020-08-16 11:45:57 -0500

    feat: get tab navigator working

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
+import Constants from 'expo-constants';
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { Provider as PaperProvider } from 'react-native-paper';
-import { StyleSheet, Text, View } from 'react-native';
+import styled from 'styled-components/native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+import WorkoutScreen from './screens/Workout';
+import EditorScreen from './screens/Editor';
+import HistoryScreen from './screens/History';
+import SettingsScreen from './screens/Settings';
+
+const StatusBarSpacer = styled.View`
+  flex: 1;
+  margin-top: ${Constants.statusBarHeight}px;
+`;
+
+const Tab = createBottomTabNavigator();
 
 export default function App(): JSX.Element {
   return (
     <PaperProvider>
-      <View style={styles.container}>
-        <Text>Open up App.tsx to start working on your app!</Text>
-        <StatusBar style="auto" />
-      </View>
+      <NavigationContainer>
+        <StatusBarSpacer>
+          <Tab.Navigator>
+            <Tab.Screen name="Workout" component={WorkoutScreen} />
+            <Tab.Screen name="Whiteboard" component={EditorScreen} />
+            <Tab.Screen name="History" component={HistoryScreen} />
+            <Tab.Screen name="Settings" component={SettingsScreen} />
+          </Tab.Navigator>
+        </StatusBarSpacer>
+      </NavigationContainer>
+      <StatusBar style="auto" />
     </PaperProvider>
   );
 }
